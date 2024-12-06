aeb13e4d3c1fc9e9bc6c19fe84843d77d4873748
Asu1996 <asutoshrt96@gmail.com>
2024-12-06 18:07:40 +0530

    feat: base code for returning sum of numbers

---
+++ findSum.js
@@ -0,0 +1,9 @@
+function sumOfNumbers(numbers) {
+  if (numbers === '') return 0
+  return numbers
+    .split(',')
+    .map(Number)
+    .reduce((acc, curr) => acc + curr, 0)
+}
+
+module.exports = sumOfNumbers
