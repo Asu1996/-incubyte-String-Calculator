aac0ba16302a4dd3302d69b42a6dbdc9aeb4e616
Asu1996 <asutoshrt96@gmail.com>
2024-12-06 19:28:06 +0530

    refactor: clean up code to prevent repetition

--- findSum.js
+++ findSum.js
@@ -1,22 +1,18 @@
 function sumOfNumbers(numbers) {
   if (numbers === '') return 0
 
+  let delimiter = ','
+
   if (numbers.startsWith('//')) {
-    const [delimiter, actualNumbers] = numbers.split('\n')
-    const actualDelimiter = delimiter.slice(2)
+    const [delimiterPart, actualNumbers] = numbers.split('\n')
+    delimiter = delimiterPart.slice(2)
 
-    return actualNumbers
-    .replace(/\n/g, actualDelimiter)
-    .split(actualDelimiter)
-    .map(Number)
-    .reduce((acc, curr) => acc + curr, 0)
-  } 
+    numbers = actualNumbers
+  }
 
-  return numbers
-    .replace(/\n/g, ',')
-    .split(',')
-    .map(Number)
-    .reduce((acc, curr) => acc + curr, 0)
+  const numbersArray = numbers.replace(/\n/g, delimiter).split(delimiter)
+
+  return numbersArray.map(Number).reduce((acc, curr) => acc + curr, 0)
 }
 
 module.exports = sumOfNumbers
